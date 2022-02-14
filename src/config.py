0b2f6a0bd5da2b7c187db0063463eb0e066db4b8
xMakona <alexdunlop999@gmail.com>
2022-02-14 08:57:14 -0500

    Changed logs/data file paths. Updated gitignore.

--- src/config.py
+++ src/config.py
@@ -1,9 +1,9 @@
 # Log file names
-BOT_LOG_FILEPATH = 'log/bot_log.txt'
+BOT_LOG_FILEPATH = '../logs/bot_log.txt'
 
 # Data file names
-CENSORED_USERS_FILEPATH = 'data/censored_users.txt'
-CENSORED_RECORDS_FILEPATH = 'data/censored_records.txt'
+CENSORED_USERS_FILEPATH = '../data/censored_users.txt'
+CENSORED_RECORDS_FILEPATH = '../data/censored_records.txt'
 
 # Default values
 DEFAULT_CENSOR_TIME = 120 # time in minutes
