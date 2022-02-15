02519ed8e2d6d747bd00afe4ca6f287c754589f3
xMakona <alexdunlop999@gmail.com>
2022-02-14 22:35:14 -0500

    Updated command help strings

--- src/command_strings.py
+++ src/command_strings.py
@@ -5,7 +5,7 @@ censor = {
 }
 sanction = {
     'name': 'sanction',
-    'help': 'No longer censors a <@user>\'s messages.',
+    'help': 'No longer censors <@user>\'s messages.',
     'usage': '<@user>'
 }
 purge = {
@@ -13,6 +13,7 @@ purge = {
     'help': ('Purges all of <@user>\'s messages found in the last <limit> messages in <#channel>. '
         'If no channels are mentioned then the channel the command is issued in is purged. '
         'If no users are mentioned, the last <limit> messages are deleted. '
-        'If no <limit> is given, defaults to last 500 messages.'),
+        'If no <limit> is given, defaults to last 500 messages.'
+        '\n\nRecommend one user/channel per purge. Discord does not like this command.'),
     'usage': '<@user> <#channel> <limit>'
 }
