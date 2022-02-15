0b89354f3f27cccd55b1444f482a0a8dcbc43fd9
xMakona <alexdunlop999@gmail.com>
2022-02-14 22:41:45 -0500

    Update command_strings.py

--- src/command_strings.py
+++ src/command_strings.py
@@ -1,11 +1,11 @@
 censor = {
     'name': 'censor',
-    'help': 'Registers <@user> for censorship, deletes each message <@user> makes <minutes> after they create it.',
+    'help': 'Registers <@user> for censorship, deletes each message <@user> makes <minutes> after they create it. Supports multiple mentions in one command.',
     'usage': '<@user> <minutes>'
 }
 sanction = {
     'name': 'sanction',
-    'help': 'No longer censors <@user>\'s messages.',
+    'help': 'No longer censors <@user>\'s messages. Supports multiple mentions in one command.',
     'usage': '<@user>'
 }
 purge = {
@@ -13,7 +13,7 @@ purge = {
     'help': ('Purges all of <@user>\'s messages found in the last <limit> messages in <#channel>. '
         'If no channels are mentioned then the channel the command is issued in is purged. '
         'If no users are mentioned, the last <limit> messages are deleted. '
-        'If no <limit> is given, defaults to last 500 messages.'
+        'If no <limit> is given, defaults to last 500 messages. Supports multiple mentions in one command.'
         '\n\nRecommend one user/channel per purge. Discord does not like this command.'),
     'usage': '<@user> <#channel> <limit>'
 }
